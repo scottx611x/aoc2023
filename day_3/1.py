60cd0c7739fbbf2c6d89b4a2e4a93ad7e30143a3
Scott Ouellette <scottx611x@gmail.com>
2023-12-04 22:09:57 -0500

    hax but also cleanup

--- day_3/1.py
+++ day_3/1.py
@@ -4,25 +4,22 @@ from typing import List
 
 start = time.time()
 
-with open("input.txt") as f:
-    data = f.readlines()
 
-
-class Matched:
+class SchematicMatch:
     def __init__(self, line_number: int, match: re.Match):
         self.match = match
         self.line_number = line_number
         self.start_pos, self.end_pos = self.match.span()
 
     def __repr__(self):
-        return f"<{self.__class__.__name__}: {self.match.group()} line: {self.line_number}, start: {self.start_pos}, end: {self.end_pos}"
+        return f"<{self.__class__.__name__}: {self.match.group()} line: {self.line_number}, start: {self.start_pos}, end: {self.end_pos}>"
 
 
-class Symbol(Matched):
+class Symbol(SchematicMatch):
     pass
 
 
-class PartNumber(Matched):
+class PartNumber(SchematicMatch):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.part_number = int(self.match.group())
@@ -32,9 +29,8 @@ class PartNumber(Matched):
             if self.line_number == symbol.line_number:
                 if self.start_pos == symbol.end_pos or symbol.start_pos == self.end_pos:
                     return True
-
             else:
-                match_bounds = list(range(self.start_pos, self.end_pos+1))
+                match_bounds = list(range(self.start_pos, self.end_pos + 1))
                 if any([symbol.start_pos in match_bounds, symbol.end_pos in match_bounds]):
                     return True
 
@@ -44,32 +40,35 @@ class PartNumber(Matched):
 schematic_lines = {}
 valid_part_numbers = set()
 
-for i, line in enumerate(data):
-    number_matches = [PartNumber(i, j) for j in list(re.finditer(r"\d+", line))]
-    symbol_matches = [Symbol(i, j) for j in list(re.finditer(r"[^0-9.\n]+", line))]
+with open("input.txt") as f:
+    data = f.readlines()
 
-    schematic_lines[i] = {
-        "number_matches": number_matches,
-        "symbol_matches": symbol_matches
+for index, schematic_line in enumerate(data):
+    schematic_lines[index] = {
+        "numbers": [PartNumber(index, part_number) for part_number in list(re.finditer(r"\d+", schematic_line))],
+        "symbols": [Symbol(index, symbol) for symbol in list(re.finditer(r"[^0-9.\n]+", schematic_line))]
     }
 
-for i in schematic_lines.keys():
-    for number_match in schematic_lines[i]["number_matches"]:
-        if number_match.is_adjacent_to_symbol(schematic_lines[i]["symbol_matches"]):
+    # Just populate the 0th schematic_lines record so that I can do this in one pass
+    if index == 0:
+        data += [""]
+        continue
+
+    index = index - 1
+
+    for number_match in schematic_lines[index]["numbers"]:
+        if number_match.is_adjacent_to_symbol(schematic_lines[index]["symbols"]):
             valid_part_numbers.add(number_match)
 
-        try:
-            if number_match.is_adjacent_to_symbol(schematic_lines[i - 1]["symbol_matches"]):
-                valid_part_numbers.add(number_match)
-        except KeyError:
-            pass
+        line_before = index - 1
+        line_after = index + 1
 
-        try:
-            if number_match.is_adjacent_to_symbol(schematic_lines[i + 1]["symbol_matches"]):
-                valid_part_numbers.add(number_match)
-        except KeyError:
-            pass
+        for line_num in [line_before, line_after]:
+            if line_num > len(data) or line_num < 0:
+                continue
 
+            if number_match.is_adjacent_to_symbol(schematic_lines[line_num]["symbols"]):
+                valid_part_numbers.add(number_match)
 
 print(sum([i.part_number for i in valid_part_numbers]))
 print('It took', time.time() - start, 'seconds.')
